ecce321c033fbceab5577e0d20780a9349860cb0
ncoderlk <ncoderlk@gmail.com>
2022-07-01 08:38:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var x = prompt("Hey, What's Your Name..?","");
+if(x!=null || x!=undefined || x!=""){
+	alert("Hey " + x + " bro, Welcome To Nithila War")
+}else{
+	return x=""
+}
+console.log("created by ncoderlk")
